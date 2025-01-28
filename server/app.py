1d69a1be40c30a0f34a7a6cbc8d4072aef2e1516
Chevy <vallchevy@gmail.com>
2025-01-28 08:50:34 -0800

    new models first pass

--- server/app.py
+++ server/app.py
@@ -12,7 +12,7 @@ from flask import make_response
 # Local imports
 from config import app, db, api
 # Add your model imports
-from models import Animal, Photograph, Location, User, Score
+from models import User, UserCard, Card, Set, Artist
 
 load_dotenv()
 
@@ -24,12 +24,7 @@ def not_found(e):
 class Users(Resource):
     def get(self):
         users = User.query.all()
-        user_dicts = []
-        for user in users:
-            user_dict = user.to_dict()
-            user_dict['scores'] = [score.score for score in user.scores]
-            user_dicts.append(user_dict)
-        return make_response(user_dicts, 200)
+        return make_response(users.to_dict(), 200)
     
     def post(self):
         data = request.json
@@ -39,132 +34,110 @@ class Users(Resource):
         db.session.commit()
         return user.to_dict(), 201
     
-class Scores(Resource):
-    def get(self):
-        scores = Score.query.all()
-        score_dicts = []
-        for score in scores:
-            score_dict = score.to_dict()
-            score_dicts.append(score_dict)
-        return make_response(score_dicts, 200)
-    
-    def post(self):
-        data = request.json  
-        user = User.query.filter_by(username=data.get('username')).first()
-        score = Score(user = user, score = data.get('score'))
-        db.session.add(score)
-        db.session.commit()
-        return score.to_dict(), 201
 
-class Animals(Resource):
-    def get(self):
-        animals = Animal.query.all()
-        animal_dicts = []
-        for animal in animals:
-            animal_dict = animal.to_dict()
-            animal_dict['locations'] = list(set([location.name for location in animal.locations]))
-            animal_dicts.append(animal_dict)
-        return make_response(animal_dicts, 200)
 
-class AnimalById(Resource):    
-    def patch(self, id):
-        print("PATCHING", id, request.json.get('description'))
-        animal = Animal.query.filter_by(id=id).first()
-        if animal:
-            try:
-                animal.name = request.json.get('name')
-                animal.description = request.json.get('description')
-            except ValueError as error:
-                print(error)
-                return {'error editing animal': [error.args[0]]}, 400
-            else:
-                db.session.commit()
-                return animal.to_dict(), 200
-        else:
-            return {'Error': 'Animal not found'}, 404
+# class Animals(Resource):
+#     def get(self):
+#         animals = Animal.query.all()
+#         animal_dicts = []
+#         for animal in animals:
+#             animal_dict = animal.to_dict()
+#             animal_dict['locations'] = list(set([location.name for location in animal.locations]))
+#             animal_dicts.append(animal_dict)
+#         return make_response(animal_dicts, 200)
+
+# class AnimalById(Resource):    
+#     def patch(self, id):
+#         print("PATCHING", id, request.json.get('description'))
+#         animal = Animal.query.filter_by(id=id).first()
+#         if animal:
+#             try:
+#                 animal.name = request.json.get('name')
+#                 animal.description = request.json.get('description')
+#             except ValueError as error:
+#                 print(error)
+#                 return {'error editing animal': [error.args[0]]}, 400
+#             else:
+#                 db.session.commit()
+#                 return animal.to_dict(), 200
+#         else:
+#             return {'Error': 'Animal not found'}, 404
         
-class Locations(Resource):
-    def get(self):
-        locations = Location.query.all()
-        location_dicts = []
-        for location in locations:
-            location_dict = location.to_dict()
-            location_dict['animals'] = list(set([animal.name for animal in location.animals]))
-            location_dicts.append(location_dict)
-        return make_response(location_dicts, 200)
+# class Locations(Resource):
+#     def get(self):
+#         locations = Location.query.all()
+#         location_dicts = []
+#         for location in locations:
+#             location_dict = location.to_dict()
+#             location_dict['animals'] = list(set([animal.name for animal in location.animals]))
+#             location_dicts.append(location_dict)
+#         return make_response(location_dicts, 200)
 
-class LocationById(Resource):
-    def patch(self, id):
-        location = Location.query.filter_by(id=id).first()
-        if location:
-            location.name = request.json.get('name')
-            location.description = request.json.get('description')
-            db.session.commit()
-            return location.to_dict(), 200
-        else:
-            return {'Error': 'Location not found'}, 404
+# class LocationById(Resource):
+#     def patch(self, id):
+#         location = Location.query.filter_by(id=id).first()
+#         if location:
+#             location.name = request.json.get('name')
+#             location.description = request.json.get('description')
+#             db.session.commit()
+#             return location.to_dict(), 200
+#         else:
+#             return {'Error': 'Location not found'}, 404
 
-class Photographs(Resource):
-    def get(self):
-        photographs = Photograph.query.all()
-        return [photograph.to_dict() for photograph in photographs], 200
+# class Photographs(Resource):
+#     def get(self):
+#         photographs = Photograph.query.all()
+#         return [photograph.to_dict() for photograph in photographs], 200
     
-    def post(self):
-        data = request.json
+#     def post(self):
+#         data = request.json
 
-        dt = datetime.now()
-        dt_formatted = f"{dt.day}/{dt.month}/{dt.year} {dt.hour}:{dt.minute:02d}"
+#         dt = datetime.now()
+#         dt_formatted = f"{dt.day}/{dt.month}/{dt.year} {dt.hour}:{dt.minute:02d}"
 
-        animal = Animal.query.filter_by(name=data.get('animal_name')).first()
-        if not animal:
-            try:
-                animal = Animal(name = data.get('animal_name'))
-            except ValueError as error:
-                print(error)
-                return {'error creating new animal': [error.args[0]]}, 400
-            db.session.add(animal)
+#         animal = Animal.query.filter_by(name=data.get('animal_name')).first()
+#         if not animal:
+#             try:
+#                 animal = Animal(name = data.get('animal_name'))
+#             except ValueError as error:
+#                 print(error)
+#                 return {'error creating new animal': [error.args[0]]}, 400
+#             db.session.add(animal)
         
-        location = Location.query.filter_by(name=data.get('location_name')).first()
-        if not location:
-            try:
-                location = Location(name = data.get('location_name'))
-            except ValueError as error:
-                return {'error creating new location': [error.args[0]]}, 400
-            db.session.add(location)
+#         location = Location.query.filter_by(name=data.get('location_name')).first()
+#         if not location:
+#             try:
+#                 location = Location(name = data.get('location_name'))
+#             except ValueError as error:
+#                 return {'error creating new location': [error.args[0]]}, 400
+#             db.session.add(location)
 
-        photograph = Photograph(datetime = dt_formatted, animal = animal, location = location, image = data.get('image'))
-        db.session.add(photograph)
-        db.session.commit()
-        return photograph.to_dict(), 201
+#         photograph = Photograph(datetime = dt_formatted, animal = animal, location = location, image = data.get('image'))
+#         db.session.add(photograph)
+#         db.session.commit()
+#         return photograph.to_dict(), 201
 
-class PhotographById(Resource):
-    def patch(self, id):
-        photograph = Photograph.query.filter_by(id=id).first()
-        if photograph:
-            photograph.animal_id = request.json.get('animal_id')
-            photograph.location_id = request.json.get('location_id')
-            db.session.commit()
-            return photograph.to_dict(), 200
-        else:
-            return {'Error': 'Photograph not found'}, 404
+# class PhotographById(Resource):
+#     def patch(self, id):
+#         photograph = Photograph.query.filter_by(id=id).first()
+#         if photograph:
+#             photograph.animal_id = request.json.get('animal_id')
+#             photograph.location_id = request.json.get('location_id')
+#             db.session.commit()
+#             return photograph.to_dict(), 200
+#         else:
+#             return {'Error': 'Photograph not found'}, 404
             
-    def delete(self, id):
-        photograph = Photograph.query.filter_by(id=id).first()
-        if photograph:
-            db.session.delete(photograph)
-            db.session.commit()
-            return {'Message': 'Photograph deleted'}, 200
-        else:
-            return {'Error': 'Photograph not found'}, 404
+#     def delete(self, id):
+#         photograph = Photograph.query.filter_by(id=id).first()
+#         if photograph:
+#             db.session.delete(photograph)
+#             db.session.commit()
+#             return {'Message': 'Photograph deleted'}, 200
+#         else:
+#             return {'Error': 'Photograph not found'}, 404
 
 api.add_resource(Users, '/_users')
-api.add_resource(Scores, '/_scores')
-api.add_resource(Animals, '/_animals')
-api.add_resource(AnimalById, '/_animals/<int:id>')
-api.add_resource(Locations, '/_locations')
-api.add_resource(LocationById, '/_locations/<int:id>')
-api.add_resource(Photographs, '/_photographs')
-api.add_resource(PhotographById, '/_photographs/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,90 +10,113 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    scores = db.relationship('Score', back_populates='user')
 
-    serialize_rules = ('-scores.user',)
+    cards = db.relationship('Card', secondary='user_cards', back_populates='users')
 
-class Score(db.Model, SerializerMixin):
-    __tablename__ = 'scores'
+class UserCard(db.Model, SerializerMixin):
+    __tablename__ = 'user_cards'
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.id'), primary_key=True)
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user = db.relationship('User', back_populates='scores')
-    score = db.Column(db.Integer)
-
-    serialize_rules = ('-user.scores',)
-
-
-class Animal(db.Model, SerializerMixin):
-    __tablename__ = 'animals'
+class Card(db.Model, SerializerMixin):
+    __tablename__ = 'cards'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
-    description = db.Column(db.String)
+    art = db.Column(db.String, nullable=False)
 
-    photographs = db.relationship('Photograph', back_populates='animal')
+    set_id = db.Column(db.Integer, db.ForeignKey('sets.id'))
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
 
-    # Adding association proxy to access locations directly from animals
-    locations = association_proxy('photographs', 'location')
+    users = db.relationship('User', secondary='user_cards', back_populates='cards')
 
-    serialize_rules = ('-photographs.animal',)
-
-    @validates('name', 'description')
-    def validate_text(self, key, value):
-        if not value:
-            raise ValueError(f'{key} cannot be empty.')
-        if type(value) != str:
-            raise ValueError(f'{key} must be a string.')
-        if key == 'description':
-            if not 10 <= len(value) <= 200:
-                raise ValueError('Description must be between 10 and 200 characters.')
-        return value
-
-
-class Photograph(db.Model, SerializerMixin):
-    __tablename__ = 'photographs'
-
-    id = db.Column(db.Integer, primary_key=True)
-    datetime = db.Column(db.String)
-    image = db.Column(db.String)
-
-    animal_id = db.Column(db.Integer, db.ForeignKey('animals.id'))
-    animal = db.relationship('Animal', back_populates='photographs')
-
-    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
-    location = db.relationship('Location', back_populates='photographs')
-
-    serialize_rules = ('-animal.photographs', '-location.photographs')
-
-    @validates('image')
-    def validate_image(self, key, value):
-        if not value:
-            raise ValueError('Photograph must include a linked image.')
-        return value
-
-
-class Location(db.Model, SerializerMixin):
-    __tablename__ = 'locations'
+class Set(db.Model, SerializerMixin):
+    __tablename__ ='sets'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
-    description = db.Column(db.String)
+    release_date = db.Column(db.Date, nullable=False)
 
-    photographs = db.relationship('Photograph', back_populates='location')
+    cards = db.relationship('Card', back_populates='set')
+    
+class Artist(db.Model, SerializerMixin):
+    __tablename__ = 'artists'
 
-    # Adding association proxy to access animals directly from locations
-    animals = association_proxy('photographs', 'animal')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
 
-    serialize_rules = ('-photographs.location',)
+    cards = db.relationship('Card', back_populates='artist')
 
-    @validates('name', 'description')
-    def validate_text(self, key, value):
-        if not value:
-            raise ValueError(f'{key} cannot be empty.')
-        if type(value) != str:
-            raise ValueError(f'{key} must be a string.')
-        if key == 'description':
-            if not 10 <= len(value) <= 200:
-                raise ValueError('Description must be between 10 and 200 characters.')
-        return value
+
+# class Animal(db.Model, SerializerMixin):
+#     __tablename__ = 'animals'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, unique=True, nullable=False)
+#     description = db.Column(db.String)
+
+#     photographs = db.relationship('Photograph', back_populates='animal')
+
+#     # Adding association proxy to access locations directly from animals
+#     locations = association_proxy('photographs', 'location')
+
+#     serialize_rules = ('-photographs.animal',)
+
+#     @validates('name', 'description')
+#     def validate_text(self, key, value):
+#         if not value:
+#             raise ValueError(f'{key} cannot be empty.')
+#         if type(value) != str:
+#             raise ValueError(f'{key} must be a string.')
+#         if key == 'description':
+#             if not 10 <= len(value) <= 200:
+#                 raise ValueError('Description must be between 10 and 200 characters.')
+#         return value
+
+
+# class Photograph(db.Model, SerializerMixin):
+#     __tablename__ = 'photographs'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     datetime = db.Column(db.String)
+#     image = db.Column(db.String)
+
+#     animal_id = db.Column(db.Integer, db.ForeignKey('animals.id'))
+#     animal = db.relationship('Animal', back_populates='photographs')
+
+#     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
+#     location = db.relationship('Location', back_populates='photographs')
+
+#     serialize_rules = ('-animal.photographs', '-location.photographs')
+
+#     @validates('image')
+#     def validate_image(self, key, value):
+#         if not value:
+#             raise ValueError('Photograph must include a linked image.')
+#         return value
+
+
+# class Location(db.Model, SerializerMixin):
+#     __tablename__ = 'locations'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, unique=True, nullable=False)
+#     description = db.Column(db.String)
+
+#     photographs = db.relationship('Photograph', back_populates='location')
+
+#     # Adding association proxy to access animals directly from locations
+#     animals = association_proxy('photographs', 'animal')
+
+#     serialize_rules = ('-photographs.location',)
+
+#     @validates('name', 'description')
+#     def validate_text(self, key, value):
+#         if not value:
+#             raise ValueError(f'{key} cannot be empty.')
+#         if type(value) != str:
+#             raise ValueError(f'{key} must be a string.')
+#         if key == 'description':
+#             if not 10 <= len(value) <= 200:
+#                 raise ValueError('Description must be between 10 and 200 characters.')
+#         return value
