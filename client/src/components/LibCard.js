f0ca19eb2604cabc8c205394336b7c258ac63ced
Chevy <vallchevy@gmail.com>
2025-03-10 19:19:04 -0700

    deleted noimage from libcard

--- client/src/components/LibCard.js
+++ client/src/components/LibCard.js
@@ -1,5 +1,4 @@
 import {useEffect, useReducer} from 'react'
-const noImage = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.shoshinsha-design.com%2Fwp-content%2Fuploads%2F2020%2F05%2Fnoimage-760x460.png&f=1&nofb=1&ipt=d872ea62b4b151bf09b2fbf210849cba33aa79c637b5c2ce34dd1d2399081e1b&ipo=images'
 
  const initialState = { owned: false }
 
