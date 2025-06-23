0b13bb29e0be526348168cbcd9e8b4a2df40a3ba
Chevy <vallchevy@gmail.com>
2025-06-23 19:44:51 -0300

    switched back to local deployment, testing clean pull

--- server/config.py
+++ server/config.py
@@ -25,11 +25,11 @@ load_dotenv()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 #comment back in to switch to local db
-# DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
-# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 
 ##Comment out to switch to local db
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
